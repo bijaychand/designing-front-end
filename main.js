f9276a60bd379e18ecc28969e7aa49dca35d2883
bijay <bijay.171349@ncit.edu.np>
2022-07-25 13:00:56 +0545

    firts commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let header=document.querySelectorq('.header');
+let menu=document.querySelector('menu');
+menu.addEventListener('click',function() {
+    header.classlist.toggle('menu-open')
+    
+})
